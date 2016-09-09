5e368b7234df10b2eff25df35fccdebe0b4a51bc
Aleksandar Kamenov <AKamenov@infragistics.com>
2016-09-09 20:22:50 +0300

    remove tic tac toe

--- 02.ResourcesViewsEventLayouts/01.TIC-TAC-TOE-Homework/app/src/main/java/com/example/akamenov/a01tic_tac_toe_homework/IRecycleViewSelectedElement.java
+++
@@ -1,9 +0,0 @@
-package com.example.akamenov.a01tic_tac_toe_homework;
-
-/**
- * Created by AKamenov on 9/8/2016.
- */
-public interface IRecycleViewSelectedElement {
-
-    void onItemSelected(int position);
-}
